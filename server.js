4b7fccd5f60b0f75022a3d7f47dd4436b3b9e9f6
StitchGen <caitlin.endrulat@gmail.com>
2020-04-25 23:41:55 -0500

    got the access token working

--- server.js
+++ server.js
@@ -1,37 +1,21 @@
+console.log("starting server.js");
 require("dotenv").config();
-
 const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken");
-
 app.use(express.json());
-
 const posts = [
-  {
-    username: "Kyle",
-    title: "Post 1",
-  },
-  {
-    username: "Jim",
-    title: "Post 2",
-  },
+  { username: "Kyle", title: "Post 1" },
+  { username: "Jim", title: "Post 2" },
 ];
 
-app.get("/posts", authenticateToken, (req, res) => {
-  res.json(posts.filter((post) => posts.username === req.user.name));
+app.get("/posts", (req, res) => {
+  res.json(posts);
 });
-
-function authenticateToken(req, res, next) {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-  if (token == null) return res.sendStatus(401);
-
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    console.log(err);
-    if (err) return res.sendStatus(403);
-    req.user = user;
-    next();
-  });
-}
-
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const user = { name: username };
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  res.json({ accessToken: accessToken });
+});
 app.listen(3000);
